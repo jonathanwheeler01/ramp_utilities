0d6ac5ce5ddbc7c449d4254b6b753cbe69e45e79
Jonathan Wheeler <jonathan.wheeler01@gmail.com>
2022-07-25 11:31:05 -0600

    add repo_id lookup utility

---
+++ scripts/repo_id_lookup.py
@@ -0,0 +1,16 @@
+import json
+
+
+with open("../supplementary_data/unified_ids.json", "r") as jd:
+    repo_ids = json.load(jd)
+
+index_lookup = input("Enter the index name of the repository: ")
+
+for key, value in repo_ids.items():
+    for k, v in value.items():
+        if index_lookup in v:
+            print(k)
+
+# u_waterloo_uwspace
+
+
