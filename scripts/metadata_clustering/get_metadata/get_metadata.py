cd58d54c4429a8119131bee546096af7d5ce7e8c
kaypounyers <95295256+kaypounyers@users.noreply.github.com>
2022-10-31 13:22:05 -0500

    get_metadata updates

--- scripts/metadata_clustering/get_metadata/get_metadata.py
+++ scripts/metadata_clustering/get_metadata/get_metadata.py
@@ -8,25 +8,64 @@ Created on Tue Oct 11 14:32:24 2022
 #import libraries
 import pandas as pd
 import sqlite3
-import numpy as np
-import os
 
-#import metadata
+# query database to collect dspace roots
+
+con = sqlite3.connect("./OneDrive/Desktop/metadata_database.db")
+cur = con.cursor()
+res = cur.execute("SELECT oairoot FROM repositories WHERE platform =  'dspace'")
+res_list= []
+res_list= res.fetchall()
+
+#clean dspace roots
+#convert from list of tuples to list of strings
+string_list = ['/'.join(str(item) for item in res_list)]
+string_list = string_list[0].split('/')
+
+#remove NONE values and create a new list of strings
+new_string = []
+for i in string_list:
+    if i != '(None,)':
+        new_string.append(i)
+    else:
+        continue
+        
+#remove characters from new string list using translate
+newest_string = []
+for i in new_string:
+    i = i.translate({ord('('): None,
+                     ord(')'): None,
+                     ord("'"): None,
+                     ord("["): None,
+                     ord("]"): None,
+                     ord(','): None})
+    newest_string.append(i)  
+# create function to grab subject metadata from the database
 def get_meta(oairoot):
-    con = sqlite3.connect('./metadata_database/repository_database.sqbpro')
+    con = sqlite3.connect("./OneDrive/Desktop/metadata_database.db")
     cur = con.cursor()
-    res = cur.execute("SELECT * FROM records WHERE id LIKE '%" + oairoot + "%'")
+    res = cur.execute("SELECT * FROM records WHERE id LIKE '%" + oairoot + "%' AND tag ='subject'" )
     res_list = []
     for r in res.fetchall():
         res_list.append(r)
     res_df = pd.DataFrame(res_list, columns=['id', 'namespace', 'tag', 'value'])
     return res_df
 
-get_meta("https://krex.k-state.edu/dspace")
+#iterate over the oairoots stored in newest_list
+metadata = pd.DataFrame(columns=['id', 'namespace','tag', 'value'])
+
+for i in newest_string:
+   result = get_meta(i)
+   metadata = pd.concat([result, metadata], axis=0)
+   print(metadata['id'].unique())
+   print(i +' is finished')
+   print (metadata.info())
+   
+   
+#reindex metadata dataframe
+metadata.reindex()
+
+#combine with clustered data
 
 
-#format metadata
-
-#export descriptive metadata
-
-#export subject metadata
+#export file
