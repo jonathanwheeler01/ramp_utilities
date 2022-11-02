2887d913afac4fa8e76457a8490b10c4b85f19e8
kaypounyers <95295256+kaypounyers@users.noreply.github.com>
2022-11-02 13:14:08 -0500

    Update get_metadata.py

--- scripts/metadata_clustering/get_metadata/get_metadata.py
+++ scripts/metadata_clustering/get_metadata/get_metadata.py
@@ -5,11 +5,11 @@ Created on Tue Oct 11 14:32:24 2022
 @author: MayeKaypounyers
 """
 
-#import libraries
+#%%import libraries
 import pandas as pd
 import sqlite3
 
-# query database to collect dspace roots
+#%% query database to collect dspace roots
 
 con = sqlite3.connect("./OneDrive/Desktop/metadata_database.db")
 cur = con.cursor()
@@ -17,34 +17,36 @@ res = cur.execute("SELECT oairoot FROM repositories WHERE platform =  'dspace'")
 res_list= []
 res_list= res.fetchall()
 
-#clean dspace roots
+#%%clean dspace roots
 #convert from list of tuples to list of strings
-string_list = ['/'.join(str(item) for item in res_list)]
-string_list = string_list[0].split('/')
+draft1_dspace_list = ['/'.join(str(item) for item in res_list)]
+draft1_dspace_list = draft1_dspace_list[0].split('/')
 
-#remove NONE values and create a new list of strings
-new_string = []
-for i in string_list:
+#%%remove NONE values and create a new list of strings
+draft2_dspace_list = []
+for i in draft1_dspace_list:
     if i != '(None,)':
-        new_string.append(i)
+        draft2_dspace_list.append(i)
     else:
         continue
         
-#remove characters from new string list using translate
-newest_string = []
-for i in new_string:
+#%%remove characters from new string list using translate
+final_dspace_list = []
+for i in draft2_dspace_list:
     i = i.translate({ord('('): None,
                      ord(')'): None,
                      ord("'"): None,
                      ord("["): None,
                      ord("]"): None,
                      ord(','): None})
-    newest_string.append(i)  
+    final_dspace_list.append(i)  
    
-#remove non-english content
-newest_string.remove
-
-# create function to grab subject metadata from the database
+#%%remove russian repositories content
+russ_repos = ['elar.urfu.ru', 'elar.uspu.ru']
+for x in russ_repos:
+    final_dspace_list.remove(x)
+    
+#%%create function to grab subject metadata from the database
 def get_meta(oairoot):
     con = sqlite3.connect("./OneDrive/Desktop/metadata_database.db")
     cur = con.cursor()
@@ -55,25 +57,19 @@ def get_meta(oairoot):
     res_df = pd.DataFrame(res_list, columns=['id', 'namespace', 'tag', 'value'])
     return res_df
 
-#iterate over the oairoots stored in newest_list
+#%%iterate over the oairoots stored in newest_list
 metadata = pd.DataFrame(columns=['id', 'namespace','tag', 'value'])
 
-for i in newest_string:
+for i in final_dspace_list:
    result = get_meta(i)
    metadata = pd.concat([result, metadata], axis=0)
-   print(metadata['id'].unique())
    print(i +' is finished')
-   print (metadata.info())
    
-   
-#reindex metadata dataframe
+#%%reindex metadata dataframe
 metadata = metadata.reset_index()
 metadata.drop('index', axis =1, inplace=True)
 metadata.info()
 
-#remove non-english content
+#%%export file
+metadata.to_csv('./subject_metadata1.csv')
 
-
-
-
-#export file
