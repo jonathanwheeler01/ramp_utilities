ad12a0fef6aefa3ffbe41a6989e650974a100926
kaypounyers <95295256+kaypounyers@users.noreply.github.com>
2022-11-16 14:53:34 -0600

    Update subject_cleaning_and_clustering.py

--- scripts/metadata_clustering/subject_cleaning_and_clustering.py
+++ scripts/metadata_clustering/subject_cleaning_and_clustering.py
@@ -10,11 +10,21 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
+
 
 #%% upload data
 data = pd.read_csv('./data/metadata_clustering_data/subject_clustering/aggregated_subject_metadata.csv') 
 
 #%% check data file
 data.head()
+data.info()
 
-#%%
+#%% apply preprocessing
+
+stop = set(stopwords.words('english'))
