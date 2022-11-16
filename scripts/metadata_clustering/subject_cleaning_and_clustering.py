b6b3b846069b70d0eb158267c0c5ed8e291a882d
kaypounyers <95295256+kaypounyers@users.noreply.github.com>
2022-11-16 15:25:40 -0600

    Update subject_cleaning_and_clustering.py

--- scripts/metadata_clustering/subject_cleaning_and_clustering.py
+++ scripts/metadata_clustering/subject_cleaning_and_clustering.py
@@ -16,6 +16,7 @@ nltk.download('omw-1.4')
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
+import string
 
 
 #%% upload data
@@ -28,3 +29,39 @@ data.info()
 #%% apply preprocessing
 
 stop = set(stopwords.words('english'))
+
+#punctuation
+exclude = set(string.punctuation)
+
+#lemmatization
+lemma = WordNetLemmatizer()
+
+#function for all cleaning steps:
+def clean(doc):
+    
+  # convert text into lower case + split into words
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    
+    # remove any punctuation
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)  
+   
+    # remove digits 
+    out_string = ''.join(i for i in punc_free if not i.isdigit())
+    
+    # normalize the text
+    normalized = " ".join(lemma.lemmatize(word) for word in out_string.split())  
+    stop_free = " ".join([i for i in normalized.lower().split() if i not in stop]) 
+
+    return stop_free
+
+data['clean_value'] = data['value'].apply(clean)
+
+#%% preview clean data
+data.head()
+
+#%% create new data file with clean data only
+clean_data = data.copy()
+del clean_data['value']
+
+#%% save clean data file for inspection
+clean_data.to_csv('./data/metadata_clustering_data/subject_clustering/clean_subject_metadata.csv')
